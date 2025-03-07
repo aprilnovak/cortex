70c25ce4d1218a418e01f369bc2b4f37a4a3d018
April Novak <novak@berkeley.edu>
2025-03-07 12:04:41 -0600

    working

--- materials/materials.py
+++ materials/materials.py
@@ -9,6 +9,12 @@ def PbLi(li6_enrichment, density):
       maximum values). Then, of the remaining material, we take Li at 0.62 weight %
       and Pb at 99.38 weight % [10.1016/j.nme.2022.101146, page 2].
 
+      We assume that this material is not depletable, since it will be flowing on
+      a timescale much faster than depletion.
+
+      TODO: non-metallic impurities (O, C, N, H) are not included, even though
+      some will remain when ingots are melted. Do we need to include these?
+
       Keyword arguments:
       li6_enrichment -- the Li6 enrichment in Li [atomic percent]
       density -- the overall density of the material [g/cc]
@@ -60,7 +66,98 @@ def PbLi(li6_enrichment, density):
 
   # total Pb concentration taken as 99.38% by weight of whatever remains after impurities
   PbLi.add_element('Pb', 0.9938 * (1 - weight_sum), 'wo')
-  print('\tLithium (weight %):    ', (1 - weight_sum) * 100)
+  print('\tLead (weight %):    ', (1 - weight_sum) * 100)
 
   PbLi.set_density('g/cc', density)
+  PbLi.depletable = False
+  return PbLi
 
+def eurofer97(density):
+  """ Return an OpenMC material for Eurofer97 RAFM steel
+      (10.1016/j.fusengdes.2018.06.027). This paper gives min, max, and target values
+      for the main alloying elements, and maximum values for impurities (radiologically
+      undesirable quantities, which may or may not be the same as other impurities
+      which are not undesirable, per se). Composition is given in weight %.
+
+      The table in the reference includes min weight %, max weight %, and target
+      weight %, but not all elements have all three numbers provided. We make the
+      following assumption:
+        - if a target % is given, we use it
+        - if only a range, between min and max is given, we take the average
+        - if only one numeric value is given, we use it
+        - if the minimum is as-low-as-possible, we take the maximum given
+        - for the As, Sn, Sb, and Zr (these are lumped together), we assume the 0.05 weight % is evenly split among these
+  """
+  eurofer97 = openmc.Material()
+  eurofer97.add_element('C', 0.11, 'wo')
+  eurofer97.add_element('Cr', 9.0, 'wo')
+  eurofer97.add_element('W', 1.1, 'wo')
+  eurofer97.add_element('Mn', 0.4, 'wo')
+  eurofer97.add_element('V', (0.25 + 0.15)/2, 'wo')
+  eurofer97.add_element('Ta', 0.12, 'wo')
+  eurofer97.add_element('N', 0.03, 'wo')
+  eurofer97.add_element('P', 0.005, 'wo')
+  eurofer97.add_element('S', 0.005, 'wo')
+  eurofer97.add_element('B', 0.002, 'wo')
+  eurofer97.add_element('O', 0.01, 'wo')
+
+  alloy_weight_sum = 0
+  for nuclide in eurofer97.nuclides:
+    alloy_weight_sum += nuclide.percent
+
+  print('\tAlloying elements (weight %): ', alloy_weight_sum)
+
+  # impurities
+  eurofer97.add_element('Nb', 0.005, 'wo')
+  eurofer97.add_element('Mo', 0.005, 'wo')
+  eurofer97.add_element('Ni', 0.01, 'wo')
+  eurofer97.add_element('Cu', 0.01, 'wo')
+  eurofer97.add_element('Al', 0.01, 'wo')
+  eurofer97.add_element('Ti', 0.02, 'wo')
+  eurofer97.add_element('Si', 0.05, 'wo')
+  eurofer97.add_element('Co', 0.01, 'wo')
+  eurofer97.add_element('As', 0.05/4, 'wo')
+  eurofer97.add_element('Sn', 0.05/4, 'wo')
+  eurofer97.add_element('Sb', 0.05/4, 'wo')
+  eurofer97.add_element('Zr', 0.05/4, 'wo')
+
+  weight_sum = 0
+  for nuclide in eurofer97.nuclides:
+    weight_sum += nuclide.percent
+
+  print('\tImpurity elements (weight %): ', weight_sum - alloy_weight_sum)
+
+  eurofer97.add_element('Fe', 100 - weight_sum, 'wo')
+
+  print('\tIron (weight %):             ', 100 - weight_sum)
+
+  eurofer97.set_density('g/cc', density)
+  return eurofer97
+
+def water(density):
+  """ Return an OpenMC material for water. TODO: add impurities
+  """
+
+  water = openmc.Material()
+  water.add_element('H', 2.0)
+  water.add_element('O', 1.0)
+  water.set_density('g/cc', density)
+  return water
+
+def helium(density):
+  """ Return an OpenMC material for helium. TODO: add impurities
+  """
+
+  helium = openmc.Material()
+  helium.add_element('He', 1.0)
+  helium.set_density('g/cc', density)
+  return helium
+
+def W(density):
+  """ Return an OpenMC material for pure tungsten.
+  """
+
+  tungsten = openmc.Material()
+  tungsten.add_element('W', 1.0)
+  tungsten.set_density('g/cc', density)
+  return tungsten
---
+++ radial_homogeneous/tile/tile.py
@@ -0,0 +1,150 @@
+import openmc
+import openmc.stats
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+import sys
+import os
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', 'materials'))
+sys.path.append(module_path)
+import materials
+
+model = openmc.Model()
+
+# OpenMC model of a bare tile with dimensions as given in the ARPA-E FOA.
+# This is the simplest model possible, and will be both (i) a lower bound
+# on our runtime and (ii) the baseline case we compare all materials against.
+thickness = 5                      # [cm] thickness of the region
+frontal_side = 50                  # [cm] side length of the tile facing the plasma
+ncells = 10                        # number of cells in the radial direction
+nwl = 1e6                          # [W/m2] neutron wall loading
+e_per_neutron = 14.07e6            # [eV] energy carried by each neutron
+
+dx = thickness / ncells
+cell_volume = frontal_side**2 * dx
+ev_to_joule = 1.60218e-19
+
+neutron_source_rate = nwl / (100**2)   # W/cm2
+neutron_source_rate /= ev_to_joule     # eV/s/cm2
+neutron_source_rate /= e_per_neutron   # neutrons/s/cm2
+neutron_source_rate *= frontal_side**2 # neutrons/s
+
+# get materials; also fetch the number of each element's atom in the cell
+# for later DPA normalization
+t = materials.W(19.254)
+
+atoms_of_element = {}
+for n in t.nuclides:
+  nuclide_mass = t.get_mass(nuclide=n.name, volume=cell_volume)
+  element = re.sub(r'[0-9]+', '', n.name)
+  n_atoms = nuclide_mass / openmc.data.atomic_mass(n.name) * 6.022e23
+  if (element in atoms_of_element):
+    atoms_of_element[element] += n_atoms
+  else:
+    atoms_of_element[element] = n_atoms
+
+model.materials = openmc.Materials([t])
+
+# define geometry; space is divided into cells for space-dependent tallies
+# using tracklength estimators for better statistics
+xplanes = [openmc.XPlane(x0=x) for x in np.linspace(0, thickness, ncells + 1)]
+xcentroids = np.linspace(dx / 2, thickness - dx/2, ncells)
+
+# TODO: figure out if I can have an actual surface source which lies on the
+# boundary of a model; right now, I have to shift the model so that the plane
+# source at x=0 is actually inside a cell
+shift = 1e-6
+xplanes[0].x0 = -shift
+
+# TODO: need to think about the boundary condition on the incident face, it
+# is not actually going to be vacuum
+xplanes[0].boundary_type = 'vacuum'
+
+xplanes[-1].boundary_type = 'vacuum'
+
+ybot = openmc.YPlane(y0=0, boundary_type = 'reflective')
+ytop = openmc.YPlane(y0=frontal_side, boundary_type = 'reflective')
+zbot = openmc.ZPlane(z0=0, boundary_type = 'reflective')
+ztop = openmc.ZPlane(z0=frontal_side, boundary_type = 'reflective')
+chop = +ybot & -ytop & +zbot & -ztop
+
+tile_cells = []
+for i in range(ncells):
+  tile_cells.append(openmc.Cell(region=+xplanes[i] & -xplanes[i+1] & chop, fill=t))
+
+root_universe = openmc.Universe(cells=tile_cells)
+model.geometry = openmc.Geometry(root=root_universe)
+
+# define the neutron source
+# TODO: use a Gaussian energy distribution instead of monoenergetic
+energy_distribution = openmc.stats.Discrete([e_per_neutron], [1])
+
+# TODO: use an isotropic angular distribution instead of all moving to the right
+angle_distribution = openmc.stats.Monodirectional()
+
+# TODO: determine if necessary to have a volume source or if this plane source
+# is sufficient; if plane source is sufficient, is the correct angle distribution
+# isotropic, or is it more like a white boundary?
+x_distribution = openmc.stats.Discrete([0], [1])
+y_distribution = openmc.stats.Uniform(a=0, b=frontal_side)
+z_distribution = openmc.stats.Uniform(a=0, b=frontal_side)
+space_distribution = openmc.stats.CartesianIndependent(x=x_distribution, y=y_distribution, z=z_distribution)
+
+model.settings = openmc.Settings()
+model.settings.source = openmc.IndependentSource(space=space_distribution, energy=energy_distribution, angle=angle_distribution)
+
+# set other model settings
+model.settings.particles = 1000
+model.settings.photon_transport = True
+model.settings.batches = 100
+model.settings.run_mode = 'fixed source'
+
+# add tallies for solution quantities
+scores = ['flux', 'heating', 'damage-energy']
+cell_filter = openmc.CellFilter(tile_cells)
+particle_filter = openmc.ParticleFilter(bins=['neutron', 'photon'])
+
+cell_tally = openmc.Tally()
+cell_tally.filters = [cell_filter, particle_filter]
+cell_tally.scores = scores
+
+model.tallies = openmc.Tallies()
+model.tallies.append(cell_tally)
+
+statepoint = model.run()
+with openmc.StatePoint(statepoint) as sp:
+  tally = sp.get_tally(id=cell_tally.id)
+  neutron_flux = tally.get_slice(scores=['flux'], filters=[type(particle_filter)], filter_bins=[('neutron',)])
+  photon_flux = tally.get_slice(scores=['flux'], filters=[type(particle_filter)], filter_bins=[('photon',)])
+  neutron_flux = neutron_flux.mean.flatten() / cell_volume * neutron_source_rate
+  photon_flux = photon_flux.mean.flatten() / cell_volume * neutron_source_rate
+
+  # create radial plots of the flux
+  plt.semilogy(xcentroids, neutron_flux, label='Neutron flux')
+  plt.semilogy(xcentroids, photon_flux, label='Photon flux')
+  plt.legend()
+  plt.grid()
+  plt.ylabel('Flux [1/cm$^2$/s]')
+  plt.xlabel('Radial Position [cm]')
+  plt.savefig('scores.png')
+  plt.close()
+
+  # create radial plots of the dpa
+  # TODO: damage-energy is only for neutrons, right?
+  dpa = tally.get_slice(scores=['damage-energy'], filters=[type(particle_filter)], filter_bins=[('neutron',)])
+
+  # TODO: get actual Ed for each nuclide, will need to generalize this for alloys.
+  Ed = 90
+  displacements_per_source = 0.8 * dpa.mean.flatten() / (2 * Ed)
+  displacements_per_s = displacements_per_source * neutron_source_rate
+  displacements_per_y = displacements_per_s * (365 * 24 * 60 * 60)
+  displacements_per_atom_per_y = displacements_per_y / atoms_of_element['W']
+  print(displacements_per_atom_per_y)
+
+  plt.semilogy(xcentroids, displacements_per_atom_per_y)
+  plt.grid()
+  plt.ylabel('DPA/y')
+  plt.xlabel('Radial Position [cm]')
+  plt.savefig('dpa.png')
+  plt.close()
