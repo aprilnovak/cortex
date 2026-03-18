7b3464d560afbe95df0ff831b6b687baa1b86b08
April Novak <novak@berkeley.edu>
2026-03-18 14:47:28 -0500

    clean up XML generation

--- detailed_models/hcll_module/make_input_xml.py
+++ detailed_models/hcll_module/make_input_xml.py
@@ -1,12 +1,89 @@
 #!/usr/bin/env python3
 
-from make_settings import *
+import openmc
+import materials as fusion_mats
 
-    
-mats_dict = createMaterials()
-createGeometry()
-createSettings()
-createTallies()
+steel = fusion_mats.ss316(7.8)
+steel.id=1
+steel.name="SS"
 
-# Make a plot...
-createPlots(mats_dict)
+#Lithium lead
+lithium_lead = fusion_mats.PbLi(0.6, 1.593)
+lithium_lead.id=2
+lithium_lead.name="PbLi"
+
+tungsten = fusion_mats.W(19.30)
+tungsten.id=3
+tungsten.name="W"
+
+pyne_mats = [ steel, lithium_lead, tungsten ]
+mats = openmc.Materials(pyne_mats)
+mats.export_to_xml()
+print("Created materials.xml")
+
+colors = ["green", "blue", "orange"]
+mats_dict = dict(zip(pyne_mats, colors))
+
+p_xz = openmc.Plot()
+p_xz.width = (1.5,0.85)
+p_xz.pixels = (1000, 600)
+p_xz.origin = (0.,0.412,0.)
+p_xz.color_by = 'material'
+p_xz.colors = mats_dict
+p_xz.basis = 'xz'
+plots = openmc.Plots([p_xz])
+plots.export_to_xml()
+print("Created plots.xml")
+
+dagmc_univ = openmc.DAGMCUniverse(filename="dagmc.h5m")
+geometry = openmc.Geometry(root=dagmc_univ)
+geometry.export_to_xml()
+
+# Specify spatial distribution for source location
+# Specify each axis direction independently
+x_disbn = openmc.stats.Uniform(-23.1,23.1)
+y_disbn = openmc.stats.Uniform(-83.5,83.5)
+z_disbn = openmc.stats.Discrete(x=[69.5],p=[1.0])
+spatial_disbn = openmc.stats.CartesianIndependent(x_disbn,y_disbn,z_disbn)
+
+# Specify an angular distribution for flux directionality
+angle_disbn = openmc.stats.Monodirectional([0.,0.,-1.])
+
+# Specify an energy distribution: 14 MeV neutrons
+energy_disbn = openmc.stats.Discrete(x=[14.0e6],p=[1.0])
+
+source = openmc.Source(space=spatial_disbn,
+                       angle=angle_disbn,
+                       energy=energy_disbn,
+                       particle='neutron')
+
+# Create settings object
+settings = openmc.Settings()
+settings.source = source
+settings.run_mode = 'fixed source'
+settings.photon_transport = False
+
+# Turn on dagmc
+settings.dagmc = True
+settings.batches = 10
+settings.particles = 1000000
+
+# add a volume calculation
+#vol_calc = openmc.VolumeCalculation()
+
+settings.export_to_xml()
+print("Created settings.xml")
+
+# Unstructured mesh to calculate tallies upon
+meshname = "HCLL-cm.e"
+umesh = openmc.UnstructuredMesh(meshname, library='libmesh')
+mesh_filter = openmc.MeshFilter(umesh)
+
+# Tallies
+tally = openmc.Tally()
+tally.filters = [mesh_filter]
+tally.scores = ['heating-local', 'flux']
+tally.estimator = 'collision'
+tallies = openmc.Tallies([tally])
+tallies.export_to_xml()
+print("Created tallies.xml")
