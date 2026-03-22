f8ac4d48040dbf6214751a587b9c7d3a69f8f698
aprilnovak <novak@berkeley.edu>
2026-03-22 10:48:22 -0500

    update plot source energy script

--- detailed_bluemira/plot_source/plot_source_energy.py
+++ detailed_bluemira/plot_source/plot_source_energy.py
@@ -2,8 +2,16 @@ import numpy as np
 import openmc
 import openmc.lib
 from tempfile import TemporaryDirectory
+import matplotlib
 import matplotlib.pyplot as plt
 import math
+
+matplotlib.rcParams.update({'font.size': 12})
+
+# Plots the NEUTRON plasma source in tandem with the surface source for comparison
+
+# Number of samples to use when sampling the source
+n_samples = 10000000
 
 from openmc_plasma_source import tokamak_source
 
@@ -28,7 +36,7 @@ my_source = tokamak_source(
     fuel={"D": 0.5, "T": 0.5},
 )
 
-def sample_initial_particles(this, n_samples: int = 1000, prn_seed: int = None):
+def sample_initial_particles(this, prn_seed: int = None):
     """smaples particles from the source.
 
     Args:
@@ -61,41 +69,84 @@ def sample_initial_particles(this, n_samples: int = 1000, prn_seed: int = None):
         )
         openmc.lib.finalize()
 
-    return particles
+        p = []
+        for i in particles:
+          if str(i.particle) == 'neutron':
+            p.append(i)
 
-# initialise a new source object
+    return p
+
+# For each source, calculate the area under the main peak to get a rough sense of
+# how different the spectrum is. This is screened by looking at bins with more
+# than 1e-3 probability AND above 10e6 MeV
+integral_tokamak = 0
+integral_slab = 0
+
+# initialise a new source object - openmc plasma source (for the tokamak)
 my_source = my_source
-
-data = sample_initial_particles(my_source, n_samples=1000000, prn_seed=1)
-
+data = sample_initial_particles(my_source, prn_seed=1)
 e_values = [particle.E for particle in data]
 
-# Calculate pdf for source energies
 energy_filter = openmc.EnergyFilter.from_group_structure('CCFE-709')
 bins = np.unique(energy_filter.bins.flatten())
 probability, bin_edges = np.histogram(e_values, bins=bins, density=True)
 
-# scaling by strength
 energy = bin_edges[:-1]
-
 unit_lethargy = energy_filter.lethargy_bin_width
 e = np.unique(bin_edges)
 
 p_times_e = [probability[i] * (e[i+1] - e[i]) for i in range(len(probability))]
+plt.step(e[:-1], p_times_e, label='From Plasma', color='k')
 
-plt.step(e[:-1], p_times_e)
-plt.xlim([1e-3, 1e8])
-plt.xscale('log')
-plt.xlabel('Neutron Energy (eV')
-plt.ylabel('Fraction of Neutrons Born in Bin')
-plt.yscale('log')
-plt.grid()
-plt.savefig('source_energy.png', bbox_inches="tight")
-plt.close()
+for i in range(len(p_times_e)):
+  if e[i] > 10e6 and p_times_e[i] > 1e-3:
+    integral_tokamak += p_times_e[i]
 
-# write data to CSV so that we can plot it alongside the slab source
 import csv
 with open('tokamak_source.csv', 'w') as csvfile:
   writer = csv.writer(csvfile)
   for i in range(len(p_times_e)):
     writer.writerow([e[i], p_times_e[i]])
+
+# initialise a new source object - surface source file
+my_source = openmc.FileSource('../surface_source.h5')
+
+data = sample_initial_particles(my_source, prn_seed=1)
+e_values = [particle.E for particle in data]
+
+energy_filter = openmc.EnergyFilter.from_group_structure('CCFE-709')
+bins = np.unique(energy_filter.bins.flatten())
+probability, bin_edges = np.histogram(e_values, bins=bins, density=True)
+
+energy = bin_edges[:-1]
+unit_lethargy = energy_filter.lethargy_bin_width
+e = np.unique(bin_edges)
+
+p_times_e = [probability[i] * (e[i+1] - e[i]) for i in range(len(probability))]
+plt.step(e[:-1], p_times_e, label='Entering Armor (WCLL)')
+
+with open('slab_source.csv', 'w') as csvfile:
+  writer = csv.writer(csvfile)
+  for i in range(len(p_times_e)):
+    writer.writerow([e[i], p_times_e[i]])
+
+for i in range(len(p_times_e)):
+  if e[i] > 10e6 and p_times_e[i] > 1e-3:
+    integral_slab += p_times_e[i]
+
+print('Tokamak, area under peak: ', integral_tokamak)
+print('Slab, area under peak: ', integral_slab)
+
+plt.xlim([1e-3, 1e8])
+plt.ylim([1e-7, 1e1])
+plt.xscale('log')
+plt.xlabel('Neutron Energy (eV)')
+plt.ylabel('Fraction of Neutrons Born in Bin')
+plt.yscale('log')
+plt.legend(loc='upper left', ncol=2)
+plt.grid()
+plt.savefig('source_energy.png', bbox_inches="tight")
+
+plt.xlim([1e7, 2e7])
+plt.savefig('source_energy_zoom.png', bbox_inches="tight")
+plt.close()
