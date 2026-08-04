56eaaa43ae97c93ad51c1d30fed21e90f1283209
gmcole2019 <gmcole2019@gmail.com>
2026-08-04 16:27:44 -0600

    add to and update element cost database

--- tea/cost_variables.py
+++ tea/cost_variables.py
@@ -20,16 +20,34 @@ class MaterialCost:
         return f"{self.name} ({self.element}): ${self.cost}/kg"
 
 material_cost_database = {
-    'B' : MaterialCost(name='Boron', element='B', cost=550),
-    'Cr' : MaterialCost(name="Chromium", element="Cr", cost=12.0),
+    "Al" : MaterialCost(name="Aluminum", element="Al", cost=3.0),
+    "As" : MaterialCost(name="Arsenic", element="As", cost=0.8),
+    'B' : MaterialCost(name='Boron', element='B', cost=550), # check
+    "C" : MaterialCost(name="Carbon", element="C", cost=10), # check
+    "Co" : MaterialCost(name="Cobalt", element="Co", cost=56),
+    'Cr' : MaterialCost(name="Chromium", element="Cr", cost=11.0),
+    "Cu" : MaterialCost(name="Copper", element="Cu", cost=14),
     "Fe" : MaterialCost(name="Iron", element="Fe", cost=1.2),
-    "Mn" : MaterialCost(name="Manganese", element="Mn", cost=2.2),
-    "Ni" : MaterialCost(name="Nickel", element="Ni", cost=22.0),
-    "Ta" : MaterialCost(name="Tantalum", element="Ta", cost=350.0),
-    "Ti" : MaterialCost(name="Titanium", element="Ti", cost=15.0),
-    "V" : MaterialCost(name="Vanadium", element="V", cost=20.0),
-    "W" : MaterialCost(name="Tungsten", element="W", cost=40.0),
-    "Y" : MaterialCost(name="Yttrium", element="Y", cost=33.0)    
+    "Mg" : MaterialCost(name="Magnesium", element="Mg", cost=2.4),
+    "Mn" : MaterialCost(name="Manganese", element="Mn", cost=1.9),
+    "Mo" : MaterialCost(name="Molybdenum", element="Mo", cost=60),
+    "N" : MaterialCost(name="SNitrogen", element="N", cost=0),
+    "Nb" : MaterialCost(name="Niobium", element="Nb", cost=90),
+    "Ni" : MaterialCost(name="Nickel", element="Ni", cost=21.0),
+    "O" : MaterialCost(name="Oxygen", element="O", cost=0),
+    "P": MaterialCost(name="Posphorous", element="P", cost=0),
+    "Pb" : MaterialCost(name="Lead", element="Pb", cost=2.0),
+    "S" : MaterialCost(name="Sulfur", element="S", cost=0),
+    "Sb" : MaterialCost(name="Antimony", element="Sb", cost=18),
+    "Si" : MaterialCost(name="Silicon", element="Si", cost=1.2),
+    "Sn" : MaterialCost(name="Tin", element="Sn", cost=54),
+    "Ta" : MaterialCost(name="Tantalum", element="Ta", cost=400.0),
+    "Ti" : MaterialCost(name="Titanium", element="Ti", cost=15.0), # check
+    "V" : MaterialCost(name="Vanadium", element="V", cost=21.0),
+    "W" : MaterialCost(name="Tungsten", element="W", cost=50.0),
+    "Y" : MaterialCost(name="Yttrium", element="Y", cost=33.0),
+    "Zn" : MaterialCost(name="Zinc", element="Zn", cost=3.4),
+    "Zr" : MaterialCost(name="Zirconium", element="Zr", cost=85)
 }
 
 # The map below was based on generic stainless steel
